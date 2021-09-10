fedf27376a8f4ce453d6b750f32dc102c819dfce
tamaracosta <tamarasantosc@gmail.com>
2021-09-10 16:13:17 -0300

    rotas iniciais e instalação react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import {  BrowserRouter,
+  Switch,
+  Route,
+  } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Notfound from './pages/NotFound';
+import Hall from './pages/Hall';
+import Kitchen from './pages/Kitchen';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-      <Route path="/login/*" element={<Login />}></Route>
-      <Route path="/" element={<Register />}></Route>
-      <Route path="*" element={<Notfound />}></Route>
-      </Routes>    
+    <Header />
+    <Switch>
+      <Route path="/" exact="true" component={Login}></Route>
+      <Route path="/login" component={Login}></Route>
+      <Route path="/register" component={Register}></Route>
+      <Route path="/hall" component={Hall}></Route>
+      <Route path="/kitchen" component={Kitchen}></Route>
+      <Route path="*" component={Notfound}></Route>
+    </Switch>
     </BrowserRouter>
   );
 }
