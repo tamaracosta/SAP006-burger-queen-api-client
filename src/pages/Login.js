6205283a1ee6bbba535dc3f28f753e5bdefba61b
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-10 17:57:03 -0300

    navegação inicial da tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,18 @@
+import Button from '../components/button/Button';
+import { Link } from 'react-router-dom';
 
 const login = () => {
     return (
         <div>
-           
             Tela inicial
-           
+            <Button variant="active">
+                <Link to="/hall">Entrar</Link>
+            </Button>
+            <Button variant="desative">
+                <Link to="/register">Cadastrar</Link>
+            </Button>
+
+
         </div>
     )
 }
