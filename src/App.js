3906860878f9811272f5ad6d3c21faf74dfcfd5a
tamaracosta <tamarasantosc@gmail.com>
2021-09-14 10:15:55 -0300

    criando página só de rota e separando do APP.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import {  BrowserRouter,
-  Switch,
-  Route,
-  } from 'react-router-dom';
 import Header from './components/Header';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Notfound from './pages/NotFound';
-import Hall from './pages/Hall';
-import Kitchen from './pages/Kitchen';
+import Routes from './Routes';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Header />
-    <Switch>
-      <Route path="/" exact="true" component={Login}></Route>
-      <Route path="/login" component={Login}></Route>
-      <Route path="/register" component={Register}></Route>
-      <Route path="/hall" component={Hall}></Route>
-      <Route path="/kitchen" component={Kitchen}></Route>
-      <Route path="*" component={Notfound}></Route>
-    </Switch>
-    </BrowserRouter>
+    <>
+      <Header />
+      <Routes />
+    </>
   );
 }
 
