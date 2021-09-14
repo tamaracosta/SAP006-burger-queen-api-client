502ef073228d8dacb3123183fd597f860d59fc7e
tamaracosta <tamarasantosc@gmail.com>
2021-09-14 11:00:12 -0300

    configurando input e botoes. CSS só para button.

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Input = ({label, id, ...props}) => {
     return (
         <div>
-        <label htmlFor={id}>{label}</label>
-        <input id={id} type="text" {...props}/>
+            <label htmlFor={id}>{label}</label>
+            <input id={id} {...props}/>
         </div>
     )
 }
--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './style.css'; 
+import './Button.css'; 
 
 const Button = (props) => {
     const batata = `abacaxi ${props.variant}`;
