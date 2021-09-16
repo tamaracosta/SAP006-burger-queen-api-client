3347e355ef2b0d9431f200d3103774de3d099006
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-16 14:23:10 -0300

    função para cadastrar usuário com api

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,29 +1,32 @@
-export const Register = (users) => {
+import { useState } from "react";
+
+export async function UserPost() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('')
+
   return fetch('https://lab-api-bq.herokuapp.com/users', {
-    method: "POST",
-      headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-       name: users.name,
-       email: users.email,
-       password: users.password,
-       role: "sample role",
-       restaurant: "Ovni Burger"
-    })
-  }
-)};
-
-
-export const loginWithEmailPassword = (users) => {
-  return  fetch("https://lab-api-bq.herokuapp.com/auth", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify ({
-      "email": users.email,
-      "password": users.password
-    })
-  });
+    body: JSON.stringify({
+      name,
+      email,
+      password,
+      role,
+      restaurant: "Ovnir Burger"
+    }),
+  }
+  ).then(response => {
+    console.log(response);
+    return response.json();
+  }).then(json => {
+    console.log(json);
+    return json
+  })
 };
+
+export default UserPost;
+
