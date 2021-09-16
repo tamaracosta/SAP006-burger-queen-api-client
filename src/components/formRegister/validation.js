130da705c8c4fce9d3eae30f8e659f4af1d82d84
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-16 14:24:21 -0300

    msg p/ usuário de validação dos campos de cadastro

--- src/components/formRegister/validation.js
+++ src/components/formRegister/validation.js
@@ -1,51 +1,28 @@
 export const ErrorsMenssage = (values) => {
   let errors = {};
-  const emailForm = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  // const passForm = !/\S+@\S+\.\S+/;
-
-  if (values.userName === '') {
+  
+  if (values.name === '') {
     errors.userName = 'Preencha seu nome';
   } 
-  // else if (!/^[A-Za-z]+/.test(values.userName)) {
-  //   errors.userName = 'Opa, verifique seu nome';
-  // }
-
-  if (values.userEmail === '') {
+  
+  if (!values.email) {
     errors.userEmail = 'Preencha seu email';
-  } else if (emailForm.test(values.userEmail)) {
+  } else if (!/\S+@\S+\.\S+/.test(values.userEmail)) {
     errors.userEmail = 'Opa, email inválido ';
   }
 
-  if (values.password === '') {
+  if (!values.password) {
     errors.password = 'Preencha seu senha'
-  } if (values.password.length < 6) {
+  } else if (values.password.length < 6) {
     errors.password = 'Sua senha deve conter mais de 6 caracteres';
   }
-  // else if (values.password === '' || values.password.length < 6) {
-  //   errors.password = 'Sua senha deve conter mais de 6 caracteres';
-  // }
 
-
-  if (!values.password2) {
-    errors.password2 = 'Por favor, verifique sua senha';
-  } else if (values.password2 !== values.password) {
-    errors.password2 = 'As senhas não batem';
+  if (!values.confirmPassword) {
+    errors.confirmPassword = 'Por favor, verifique sua senha';
+  } else if (values.confirmPassword !== values.password) {
+    errors.confirmPassword = 'As senhas não batem';
   }
   return errors;
 }
 
 export default ErrorsMenssage;
-
-// switch (!values) {
-//   case 'error': 
-//   errors.userName = 'Nome inválido'
-//   break 
-
-//   case 'error1': 
-//   errors.userEmail = 'Email inválido'
-//   break
-
-
-
-//   default:
-// }
