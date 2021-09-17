7d237b627fab5be7209e57c00becc6f2653007eb
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-17 20:35:43 -0300

    campos inseridos e refatoração

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,6 +2,7 @@ import useForm from "./useForm";
 import Input from "../../components/input/Input";
 import Button from "../../components/button/Button";
 import ErrorsMessage from "./ValidationForm";
+import { Link } from 'react-router-dom';
 
 const LoginDeus = () => {
   const { handleChange, handleSubmit, errors } = useForm(ErrorsMessage);
@@ -15,15 +16,19 @@ const LoginDeus = () => {
           type="text"
           onChange={handleChange}
         />
-        {errors.email && <p className="msgErrs">{errors.email}</p>}
+        {errors.email && <p>{errors.email && errors.email}</p>}
+
         <label htmlFor="email">Senha</label>
         <Input
           name="password"
           type="password"
           onChange={handleChange}
         />
+        <p className="errorMessage">{errors.password && errors.password}</p>
+
         <Button onClick={handleSubmit}>Entrar</Button>
-  
+        <p>ou</p>
+            <p>Primeira vez? <Link to="/register">Cadastre-se aqui</Link></p>
       </form>
     </section>
   )
