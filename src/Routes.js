5cfb644f44b4d02dc1bce3b9b76f7b9205f1bf40
tamaracosta <tamarasantosc@gmail.com>
2021-09-17 14:52:26 -0300

    rota privada e exclusao home.js e login.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,32 @@
-import {  BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import {  BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Login from './pages/Login/Login';
 import Register from './pages/Register';
 import Notfound from './pages/NotFound';
 import Hall from './pages/Hall';
 import Kitchen from './pages/Kitchen';
 
+
 const Routes = () => {
+
+    const PrivateRoute = (props) => {
+        const token = localStorage.getItem("token");
+        return token ? (
+          <Route {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+        );
+      };
+
+   
     return (
-        <BrowserRouter>
+        <BrowserRouter> 
             <Switch>
-                <Route path="/" exact component={Login}></Route>
-                <Route path="/login" component={Login}></Route>
-                <Route path="/register" component={Register}></Route>
-                <Route path="/hall" component={Hall}></Route>
-                <Route path="/kitchen" component={Kitchen}></Route>
-                <Route path="*" component={Notfound}></Route>
+                <Route path="/" exact component={Login} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <PrivateRoute path="/hall" component={Hall} />
+                <PrivateRoute path="/kitchen" component={Kitchen} />
+                <Route path="*" component={Notfound} />
             </Switch>
         </BrowserRouter>
     )
