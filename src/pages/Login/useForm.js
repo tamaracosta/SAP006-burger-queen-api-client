bf738ed538b406c765494d246f6dc8d02e977ac3
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-17 20:33:15 -0300

    refatoração

--- src/pages/Login/useForm.js
+++ src/pages/Login/useForm.js
@@ -1,21 +1,20 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom'
 import { LoginWithEmailPassword } from '../../services/auth';
-// import ErrorsMessage from '../../services/ValidationForm.js';
 
 const useForm = (validation) => {
+  const clearToken =  localStorage.clear();
   const [messageError, setMessageError] = useState('');
 
   const [values, setValues] = useState({
     email: '',
     password: '',
   });
-  
+
   const [errors, setErrors] = useState({
     email: '',
     password: '',
   });
-
-  // const [submitting, setSubmitting] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target
@@ -25,20 +24,42 @@ const useForm = (validation) => {
     });
   };
 
+  const history = useHistory()
+
+  const routerHall = () => {
+    history.push('/hall')
+  }
+  const routerKitchen = () => {
+    history.push('/kitchen')
+  }
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     (setErrors(validation(values)));
     console.log(values);
-  
-    LoginWithEmailPassword(values.email, values.password)
+
+    if(errors) {
+      LoginWithEmailPassword(values.email, values.password)
         .then((response) => {
 
           if (response.code && response.code === 400) {
-            alert(response.message)
+            console.log(response.message)
           } else {
-            alert('ok' + response.token);
+            console.log('ok' + response.token);
             localStorage.setItem('token', response.token);
+
+            const id = response.id
+            localStorage.setItem("id", id)
+
+            if (response.role === "hall") {
+              routerHall();
+              clearToken();
+            } if (response.role === "kitchen") {
+              routerKitchen();
+              clearToken();
+            }
           }
 
         })
@@ -46,11 +67,7 @@ const useForm = (validation) => {
           setMessageError('Erro na requisição. [' + error.message + ']');
           console.log(messageError)
         })
-
-    // (setErrors(validation(values)));
-    // console.log(values);
-    // setSubmitting(true);
-    // conecção com api deve ser aqui
+      }
   };
 
   return { handleChange, handleSubmit, errors };
