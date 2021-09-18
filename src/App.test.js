03b27c3247807aa814cd99d4534725d12ef4d514
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-18 19:56:51 -0300

    testes unitários do button e input

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,72 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render } from '@testing-library/react';
+import Button from '../src/components/button/Button';
+import Input from '../src/components/input/Input';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+// button 
+test('click button in screen login', done => {
+  function handleSubmit() {
+    done();
+}
+  const { getByText } = render(
+    <Button onClick={handleSubmit}>Entrar</Button>);
+  const btnClick = getByText('Entrar');
+  fireEvent.click(btnClick);
 });
+
+test('click button in screen register', done => {
+  function handleSubmit() {
+    done();
+  }
+  const { getByText } = render(
+    <Button onClick={handleSubmit}>Cadastre-se</Button>);
+  const btnRegister = getByText('Cadastre-se');
+  fireEvent.click(btnRegister);
+});
+
+// input
+test('selected attendant', done => {
+  function handleChange(e) {
+    jest.setTimeout(30000)
+    expect(e.target.value).toEqual('atendente');
+    done();
+  }
+  const { getByPlaceholderText } = render(
+    <Input onChange={handleChange} placeholder="atendente" />);
+  const iptAttendant = getByPlaceholderText('atendente');
+  fireEvent.change(iptAttendant, { target: {value: "atendente"}});
+});
+
+test('selected cook', done => {
+  function handleChange(e) {
+    expect(e.target.value).toEqual('cozinheiro');
+    done();
+  }
+  const { getByPlaceholderText } = render(
+    <Input onChange={handleChange} placeholder="cozinheiro" />);
+  const iptCook = getByPlaceholderText('cozinheiro');
+  fireEvent.change(iptCook, { target: {value: "cozinheiro"}});
+});
+
+
+
+
+
+
+
+
+//test('renders learn react link', () => {
+  //   render(<Input />);
+  // const linkElement = screen.getByText(/aqui/i);
+  // expect(linkElement).toBeInTheDocument();
+  //   console.log(linkElement);
+  // });
+  
+  // import { render, screen } from '@testing-library/react';
+  // import Button from './components/button/Button';
+  
+  // describe('Button component', () => {
+  //   describe('button render', () => {
+  //     const label = 'voabebe'
+  //     render(<Button> {label} </Button>);
+  
