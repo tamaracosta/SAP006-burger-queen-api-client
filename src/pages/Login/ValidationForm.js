606beb41d11d2b2b214a11c02ddbae76e048b06e
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-09-22 15:04:17 -0300

    apagado condições que não precisavam

--- src/pages/Login/ValidationForm.js
+++ src/pages/Login/ValidationForm.js
@@ -1,8 +1,8 @@
 export const ErrorsMessage = (values) => {
   let errors = {};
   
-  if (values.name === '') {
-    errors.userName = 'Preencha seu nome';
+  if (!values.name) {
+    errors.name = 'Preencha seu nome';
   } 
   
   if (!values.email) {
@@ -11,17 +11,6 @@ export const ErrorsMessage = (values) => {
     errors.email = 'Opa, email inválido ';
   }
 
-  if (!values.password) {
-    errors.password = 'Preencha seu senha'
-  } else if (values.password.length < 6) {
-    errors.password = 'Sua senha deve conter mais de 6 caracteres';
-  }
-
-  if (!values.confirmPassword) {
-    errors.confirmPassword = 'Por favor, verifique sua senha';
-  } else if (values.confirmPassword !== values.password) {
-    errors.confirmPassword = 'As senhas não batem';
-  }
   return errors;
 }
 
