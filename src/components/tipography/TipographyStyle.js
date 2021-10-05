3f978b964ff2768cfecb390a3e6a406b106929a9
tamaracosta <tamarasantosc@gmail.com>
2021-10-05 11:51:47 -0300

    acrescentando Tipografia DefaultTitle

--- src/components/tipography/TipographyStyle.js
+++ src/components/tipography/TipographyStyle.js
@@ -11,7 +11,19 @@ export const SecundaryParagraph = styled(Paragraph) `
 color: red;
 `
 
-export const LinkStyle = styled.a`
+export const LinkStyle = styled.span`
 color: var(--yellow);
 
+`
+
+export const DefaultTitle = styled.p`
+text-align: center;
+font-size: 1.5em;
+margin: 0.7em;
+color: var(--light-white);
+
+  @media screen and (min-width: 1024px) {
+    font-size: 1.2em;
+              
+  }
 `
